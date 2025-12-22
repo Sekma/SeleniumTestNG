a95c776f1c617b1a4a5935fdea5cd423ef1d159f
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2025-12-22 20:07:48 +0100

    first commit

--- src/main/java/com/todos/pages/TodoPage.java
+++ src/main/java/com/todos/pages/TodoPage.java
@@ -2,8 +2,10 @@ package com.todos.pages;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
@@ -12,38 +14,66 @@ import com.todos.utils.BasePage;
 
 public class TodoPage extends BasePage {
 
+    /**Elements**/
 	@FindBy(how = How.ID, using = "todo-input")
 	public static WebElement inputText;
-	@FindBy(how = How.CSS, using = "label[data-testid='todo-item-label'] ")
-	public static WebElement todosText;
-	@FindBy(how = How.XPATH, using = "//input[@class=\"toggle\"]")
-	public static WebElement checkBox;
-
+	
+	/**Methods**/
 	public TodoPage() throws IOException {
 		PageFactory.initElements(driver, this);
 	}
-
-	public void submitTodo(String todos) {
+	// Method to generate todos
+	public String todoToSubmit(int num) {
+		String todo = prop.getProperty("todos."+num);
+		return todo;
+	}
+	
+	// Method to find todo By Text
+	public WebElement todoText(int num) {		
+	    return driver.findElement(By.xpath("//li//label[text()='"+todoToSubmit(num)+"']")
+	    );
+	}
+	// Method to find checkBox By Text
+	public WebElement checkBox(int num) {
+	    return driver.findElement(By.xpath("//label[text()='"+todoToSubmit(num)+"']/preceding-sibling::input[@type='checkbox']")
+	    );
+	}
+	// Method to find destroyButton By Text
+	public WebElement destroyButton(int num) {
+	    return driver.findElement(By.xpath("//label[text()='"+todoToSubmit(num)+"']/following-sibling::button[contains(@class,'destroy')]")
+	    );
+	}
+	
+	
+	// Method to verify if the input is displayed
+	public boolean isElementDisplayed(WebElement element) {
+		boolean iselementdisplayed = element.isDisplayed();
+		return iselementdisplayed;
+	}
+	// Method to enter a new todo
+	public void submitTodo(int num) {
 		inputText.clear();
-		inputText.sendKeys(todos);
+		inputText.sendKeys(todoToSubmit(num));
 		inputText.sendKeys(Keys.ENTER);
 	}
 
-	public boolean iselementdisplayed(WebElement element) {
-		boolean iselementdisplayed = element.isDisplayed();
-		return iselementdisplayed;
-
+    // Method to get text for todo element
+	public String checkElementContaint(int num) {
+		String elementTodo = todoText(num).getText();
+		return elementTodo;
+	}
+	// Method to verify checkbox of todo
+	public boolean isCheckBoxSelected(WebElement element) {
+		boolean isCheckBoxSelected = element.isSelected();
+		return isCheckBoxSelected;
+	}
+	
+	public WebElement destroyElement(int num) {
+		// hover on element todosText to display the destroyButton.
+		Actions action = new Actions(driver);
+		action.moveToElement(todoText(num)).perform();
+		// find the button
+		 return destroyButton(num);
 	}
 
-	public String checkElemenTContain(WebElement element) {
-		String elementtodo = element.getText();
-		return elementtodo;
-
-	}
-
-	public boolean ischeckboxselected(WebElement element) {
-		boolean ischeckboxselected = element.isSelected();
-		return ischeckboxselected;
-
-	}
 }
--- src/test/java/com/todos/test/AddTodoTest.java
+++ src/test/java/com/todos/test/AddTodoTest.java
@@ -17,14 +17,22 @@ public class AddTodoTest extends Setup {
 	@Test(priority = 1)
 	public void IcanAddTodo() throws IOException {
 		todopage = new TodoPage();
-		boolean todofield = todopage.iselementdisplayed(TodoPage.inputText);
-		Assert.assertTrue(todofield);
-		todopage.submitTodo(prop.getProperty("todos1"));
-		String elementtodo = todopage.checkElemenTContain(TodoPage.todosText);
-		Assert.assertTrue(elementtodo.contains(prop.getProperty("todos1")));
-		boolean checkbox = todopage.ischeckboxselected(TodoPage.checkBox);
-		Assert.assertFalse(checkbox);
-
+		
+		boolean todoField = todopage.isElementDisplayed(TodoPage.inputText);
+		Assert.assertTrue(todoField);
+		// enter todo 1
+		todopage.submitTodo(1);
+		String elementTodo = todopage.checkElementContaint(1);
+		Assert.assertTrue(elementTodo.contains(todopage.todoToSubmit(1)));
+		// verify is it not checked by default
+		boolean checkBox = todopage.isCheckBoxSelected(todopage.checkBox(1));
+		Assert.assertFalse(checkBox);
+		
+		// enter todo 2 and todo 3
+		todopage.submitTodo(2);
+		todopage.submitTodo(3);
+		// delete todo 2
+		todopage.destroyElement(2).click();
 	}
 
 }
